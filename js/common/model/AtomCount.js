3f2e1a0230140f3fe07bd31b676722ba14668cb7
Chris Malley <cmalley@pixelzoom.com>
2014-06-17 14:46:05 -0600

    add missing constructor doc

--- js/common/model/AtomCount.js
+++ js/common/model/AtomCount.js
@@ -1,6 +1,5 @@
 // Copyright 2002-2014, University of Colorado Boulder
 
-
 /**
  * Data structure for describing how many times an atom appears in an equation.
  * There are separate counts for the left-hand (reactants) and right-hand (products)
@@ -8,10 +7,15 @@
  *
  * @author Vasily Shakhov (cmalley@pixelzoom.com)
  */
-
 define( function() {
   'use strict';
 
+  /**
+   * @param {nitroglycerin.Element} element
+   * @param {Number} reactantsCount
+   * @param {Number} productsCount
+   * @constructor
+   */
   var AtomCount = function( element, reactantsCount, productsCount ) {
     // the element that describes the atom's chemical properties
     this.element = element;
