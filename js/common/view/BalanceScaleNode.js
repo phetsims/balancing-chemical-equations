e9e48beca7a4c25646eadb6e97410bf9904ea9ce
Sam Reid <reids@colorado.edu>
2015-10-06 09:55:53 -0600

    Fixed lint issues, see https://github.com/phetsims/chipper/issues/235

--- js/common/view/BalanceScaleNode.js
+++ js/common/view/BalanceScaleNode.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2014, University of Colorado
+// Copyright 2002-2014, University of Colorado Boulder
 
 /**
  * A balance scale, depicts the relationship between the atom count
--- js/common/view/HorizontalAligner.js
+++ js/common/view/HorizontalAligner.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2014, University of Colorado
+// Copyright 2002-2014, University of Colorado Boulder
 
 /**
  * Encapsulates the strategy used to horizontally aligning terms in an equation
