477e8ad4ab11e3a33585796d34d3f3ffd8d6ec60
Chris Malley <cmalley@pixelzoom.com>
2016-08-18 17:24:04 -0600

    format code

--- js/common/BCEQueryParameters.js
+++ js/common/BCEQueryParameters.js
@@ -14,17 +14,18 @@ define( function( require ) {
   var getQueryParameter = phet.chipper.getQueryParameter;
 
   var BCEQueryParameters = {
+
     // enabled developer-only features
-    DEV:      getQueryParameter( 'dev' ) || false,
+    DEV: getQueryParameter( 'dev' ) || false,
 
     // play all challenges for each level of the game, to get 100% test coverage
     PLAY_ALL: getQueryParameter( 'playAll' ) || false,
 
     // show the game reward regardless of score
-    REWARD:   getQueryParameter( 'reward' ) || false,
+    REWARD: getQueryParameter( 'reward' ) || false,
 
     // enables console output for debugging, particularly useful for the Game screen
-    CONSOLE:  getQueryParameter( 'console' ) || false
+    CONSOLE: getQueryParameter( 'console' ) || false
   };
 
   balancingChemicalEquations.register( 'BCEQueryParameters', BCEQueryParameters );
