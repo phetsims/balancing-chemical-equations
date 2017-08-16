89ac16103cda6db1343e402248a7aa81ac066fa4
zepumph <michael.kauzmann@colorado.edu>
2017-08-15 16:39:02 -0800

    remove unnecessary newlines or replace with <br>, https://github.com/phetsims/joist/issues/435

--- js/balancing-chemical-equations-main.js
+++ js/balancing-chemical-equations-main.js
@@ -19,10 +19,12 @@ define( function( require ) {
     credits: {
       leadDesign: 'Kelly Lancaster (Java), Yuen-ying Carpenter (HTML5)',
       softwareDevelopment: 'Chris Malley (PixelZoom, Inc.)',
-      team: 'Julia Chamberlain, Patricia Loeblein, Emily B. Moore,\nRobert Parson, Ariel Paul, Kathy Perkins',
-      qualityAssurance: 'Steele Dalton, Bryce Griebenow, Elise Morgan,\nOliver Orejola, Benjamin Roberts, Bryan Yoelin',
-      thanks: '\u2022 Conversion of this simulation to HTML5 was funded in part by\nthe American Association of Chemistry Teachers (AACT).\n' +
-              '\u2022 Thanks to Mobile Learner Labs for working with the PhET\ndevelopment team to convert this simulation to HTML5.'
+      team: 'Julia Chamberlain, Patricia Loeblein, Emily B. Moore. Robert Parson, Ariel Paul, Kathy Perkins',
+      qualityAssurance: 'Steele Dalton, Bryce Griebenow, Elise Morgan. Oliver Orejola, Benjamin Roberts, Bryan Yoelin',
+      thanks: '\u2022 Conversion of this simulation to HTML5 was funded in part b. the American Association ' +
+              'of Chemistry Teachers (AACT). <br>' +
+              '\u2022 Thanks to Mobile Learner Labs for working with the PhE. development team to convert this ' +
+              'simulation to HTML5.'
     }
   };
 
