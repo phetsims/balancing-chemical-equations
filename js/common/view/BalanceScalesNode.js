d1befd14b03fc2fae4c9dc7e640b2f8940a5908c
samreid <reids@colorado.edu>
2018-11-19 12:55:04 -0700

    Update copyright dates, see https://github.com/phetsims/chipper/issues/711

--- js/common/view/BalanceScalesNode.js
+++ js/common/view/BalanceScalesNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2017, University of Colorado Boulder
+// Copyright 2014-2018, University of Colorado Boulder
 
 /**
  * Visual representation of an equation as a set of balance scales, one for each atom type.
--- js/game/view/LevelSelectionNode.js
+++ js/game/view/LevelSelectionNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2017, University of Colorado Boulder
+// Copyright 2014-2018, University of Colorado Boulder
 
 /**
  * Controls for selecting a level and adjusting various game settings (sound, timer, ...)
