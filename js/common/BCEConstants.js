63a9a145cbc00c89fa28081134f042ca58ee2185
Chris Malley <cmalley@pixelzoom.com>
2018-12-13 10:30:28 -0700

    fix leak of fontawesome icons, #134
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/common/BCEConstants.js
+++ js/common/BCEConstants.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2017, University of Colorado Boulder
+// Copyright 2014-2018, University of Colorado Boulder
 
 /**
  * A collection of constants that configure global properties.
@@ -12,7 +12,6 @@ define( function( require ) {
   // modules
   var balancingChemicalEquations = require( 'BALANCING_CHEMICAL_EQUATIONS/balancingChemicalEquations' );
   var Bounds2 = require( 'DOT/Bounds2' );
-  var FontAwesomeNode = require( 'SUN/FontAwesomeNode' );
 
   var BCEConstants = Object.freeze( {
     SCREEN_VIEW_OPTIONS: { layoutBounds: new Bounds2( 0, 0, 768, 504 ) },
@@ -22,9 +21,7 @@ define( function( require ) {
     GAME_CANVAS_BACKGROUND: '#ffffe4',
     BOX_COLOR: 'white',
     ATOM_OPTIONS: { stroke: 'black', lineWidth: 0.5 },
-    MOLECULE_SCALE_FACTOR: 0.74, // scale all molecules by scaleFactor to fit design
-    CORRECT_ICON: new FontAwesomeNode( 'check', { fill: 'rgb( 0, 180, 0 )' } ), // check mark
-    INCORRECT_ICON: new FontAwesomeNode( 'times', { fill: 'rgb(252,104,0)' } ) // X
+    MOLECULE_SCALE_FACTOR: 0.74 // scale all molecules by scaleFactor to fit design
   } );
 
   balancingChemicalEquations.register( 'BCEConstants', BCEConstants );
