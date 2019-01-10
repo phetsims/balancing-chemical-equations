7d030bfcdefb2b81e6e7add53b38f53372b85a42
Chris Malley <cmalley@pixelzoom.com>
2019-01-10 13:37:43 -0700

    fix typo in credits, #132
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/balancing-chemical-equations-main.js
+++ js/balancing-chemical-equations-main.js
@@ -19,7 +19,7 @@ define( function( require ) {
     credits: {
       leadDesign: 'Kelly Lancaster (Java), Yuen-ying Carpenter (HTML5)',
       softwareDevelopment: 'Chris Malley (PixelZoom, Inc.)',
-      team: 'Julia Chamberlain, Patricia Loeblein, Emily B. Moore. Robert Parson, Ariel Paul, Kathy Perkins, Amy Rouinfar',
+      team: 'Julia Chamberlain, Patricia Loeblein, Emily B. Moore, Robert Parson, Ariel Paul, Kathy Perkins, Amy Rouinfar',
       qualityAssurance: 'Steele Dalton, Bryce Griebenow, Elise Morgan, Oliver Orejola, Benjamin Roberts, Bryan Yoelin',
       thanks: '\u2022 Conversion of this simulation to HTML5 was funded in part by the American Association of Chemistry Teachers (AACT).<br>' +
               '\u2022 Thanks to Mobile Learner Labs for working with the PhET development team to convert this simulation to HTML5.'
