f04bd5b71c0ba71470299b2322e0745337a5d81b
Chris Malley <cmalley@pixelzoom.com>
2019-01-19 21:06:32 -0700

    convert ComboBox and its subtypes to class, https://github.com/phetsims/sun/issues/456

--- js/introduction/view/ToolsComboBox.js
+++ js/introduction/view/ToolsComboBox.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2017, University of Colorado Boulder
+// Copyright 2014-2019, University of Colorado Boulder
 
 /**
  * 'Tools' combo box, for selecting the visual representation for "balanced".
@@ -10,64 +10,61 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var BalancedRepresentation = require( 'BALANCING_CHEMICAL_EQUATIONS/common/model/BalancedRepresentation' );
-  var balancingChemicalEquations = require( 'BALANCING_CHEMICAL_EQUATIONS/balancingChemicalEquations' );
-  var ComboBox = require( 'SUN/ComboBox' );
-  var ComboBoxItem = require( 'SUN/ComboBoxItem' );
-  var Image = require( 'SCENERY/nodes/Image' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
-  var Text = require( 'SCENERY/nodes/Text' );
+  const BalancedRepresentation = require( 'BALANCING_CHEMICAL_EQUATIONS/common/model/BalancedRepresentation' );
+  const balancingChemicalEquations = require( 'BALANCING_CHEMICAL_EQUATIONS/balancingChemicalEquations' );
+  const ComboBox = require( 'SUN/ComboBox' );
+  const ComboBoxItem = require( 'SUN/ComboBoxItem' );
+  const Image = require( 'SCENERY/nodes/Image' );
+  const PhetFont = require( 'SCENERY_PHET/PhetFont' );
+  const Text = require( 'SCENERY/nodes/Text' );
 
   // images
-  var chartsImage = require( 'image!BALANCING_CHEMICAL_EQUATIONS/charts.png' );
-  var scalesImage = require( 'mipmap!BALANCING_CHEMICAL_EQUATIONS/scales.png' );
+  const chartsImage = require( 'image!BALANCING_CHEMICAL_EQUATIONS/charts.png' );
+  const scalesImage = require( 'mipmap!BALANCING_CHEMICAL_EQUATIONS/scales.png' );
 
   // strings
-  var noneString = require( 'string!BALANCING_CHEMICAL_EQUATIONS/none' );
-  var toolsString = require( 'string!BALANCING_CHEMICAL_EQUATIONS/tools' );
+  const noneString = require( 'string!BALANCING_CHEMICAL_EQUATIONS/none' );
+  const toolsString = require( 'string!BALANCING_CHEMICAL_EQUATIONS/tools' );
 
   // constants
-  var FONT = new PhetFont( 22 );
+  const FONT = new PhetFont( 22 );
 
-  /**
-   * @param {Property.<BalancedRepresentation>} balanceRepresentationProperty
-   * @param {Node} parentNode node that will be used as the list's parent, use this to ensuring that the list is in front of everything else
-   * @param {Object} [options]
-   * @constructor
-   */
-  function ToolsComboBox( balanceRepresentationProperty, parentNode, options ) {
+  class ToolsComboBox extends ComboBox {
 
-    options = _.extend( {
-      xMargin: 10,
-      yMargin: 5,
-      cornerRadius: 4,
-      maxWidth: 600
-    }, options );
+    /**
+     * @param {Property.<BalancedRepresentation>} balanceRepresentationProperty
+     * @param {Node} parentNode node that will be used as the list's parent, use this to ensuring that the list is in front of everything else
+     * @param {Object} [options]
+     * @constructor
+     */
+    constructor( balanceRepresentationProperty, parentNode, options ) {
 
-    // options that cannot be specified by client
-    options.labelNode = new Text( toolsString, {
-      font: FONT,
-      fontWeight: 'bold',
-      maxWidth: 100
-    } );
+      options = _.extend( {
+        xMargin: 10,
+        yMargin: 5,
+        cornerRadius: 4,
+        maxWidth: 600
+      }, options );
 
-    var items = [
-      // 'None'
-      new ComboBoxItem( new Text( noneString, { font: FONT, maxWidth: 100 } ), BalancedRepresentation.NONE ),
-      // scales
-      new ComboBoxItem( new Image( scalesImage, { scale: 0.1875 } ), BalancedRepresentation.BALANCE_SCALES ),
-      // bar charts
-      new ComboBoxItem( new Image( chartsImage, { scale: 0.375 } ), BalancedRepresentation.BAR_CHARTS )
-    ];
+      // options that cannot be specified by client
+      options.labelNode = new Text( toolsString, {
+        font: FONT,
+        fontWeight: 'bold',
+        maxWidth: 100
+      } );
 
-    ComboBox.call( this, items, balanceRepresentationProperty, parentNode, options );
+      const items = [
+        // 'None'
+        new ComboBoxItem( new Text( noneString, { font: FONT, maxWidth: 100 } ), BalancedRepresentation.NONE ),
+        // scales
+        new ComboBoxItem( new Image( scalesImage, { scale: 0.1875 } ), BalancedRepresentation.BALANCE_SCALES ),
+        // bar charts
+        new ComboBoxItem( new Image( chartsImage, { scale: 0.375 } ), BalancedRepresentation.BAR_CHARTS )
+      ];
+
+      super( items, balanceRepresentationProperty, parentNode, options );
+    }
   }
 
-  balancingChemicalEquations.register( 'ToolsComboBox', ToolsComboBox );
-
-  return inherit( ComboBox, ToolsComboBox, {
-
-    // Supertype implements dispose, but we don't need to call it, this type persists for the lifetime of the sim.
-  } );
+  return balancingChemicalEquations.register( 'ToolsComboBox', ToolsComboBox );
 } );
