52f42383aa0f9f1b8b6d357a9afccc521cda1067
phet-dev <phetadmin@gmail.com>
2020-02-27 03:07:33 -0700

    update copyright dates from daily grunt work

--- js/common/view/TermNode.js
+++ js/common/view/TermNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2019, University of Colorado Boulder
+// Copyright 2014-2020, University of Colorado Boulder
 
 /**
  * A term in the equation, includes the coefficient and symbol.
--- js/game/view/LevelSelectionNode.js
+++ js/game/view/LevelSelectionNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2019, University of Colorado Boulder
+// Copyright 2014-2020, University of Colorado Boulder
 
 /**
  * Controls for selecting a level and adjusting various game settings, such as whether the timer is enabled
