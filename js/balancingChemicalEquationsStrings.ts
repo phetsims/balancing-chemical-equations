27680a7ddff3d5805bb8357800765436a64ed295
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:23 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/balancingChemicalEquationsStrings.ts
@@ -0,0 +1,43 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import balancingChemicalEquations from './balancingChemicalEquations.js';
+
+type StringsType = {
+  'none': string,
+  'balanced': string,
+  'screen': {
+    'introduction': string,
+    'game': string
+  },
+  'separateWater': string,
+  'hideWhy': string,
+  'showAnswer': string,
+  'tryAgain': string,
+  'notBalanced': string,
+  'balancing-chemical-equations': {
+    'title': string
+  },
+  'combustMethane': string,
+  'next': string,
+  'check': string,
+  'makeAmmonia': string,
+  'showWhy': string,
+  'notSimplified': string,
+  'tools': string,
+  'reactants': string,
+  'products': string,
+  'chooseYourLevel': string,
+  'pattern_0level': string,
+  'pattern_0points': string
+};
+
+const balancingChemicalEquationsStrings = getStringModule( 'BALANCING_CHEMICAL_EQUATIONS' ) as StringsType;
+
+balancingChemicalEquations.register( 'balancingChemicalEquationsStrings', balancingChemicalEquationsStrings );
+
+export default balancingChemicalEquationsStrings;
