0053574dbfa90f37cdf2993f906d5304d14be425
Sam Reid <reids@colorado.edu>
2022-05-07 09:33:09 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/balancingChemicalEquationsStrings.ts
+++ js/balancingChemicalEquationsStrings.ts
@@ -8,32 +8,32 @@ import getStringModule from '../../chipper/js/getStringModule.js';
 import balancingChemicalEquations from './balancingChemicalEquations.js';
 
 type StringsType = {
-  'none': string,
-  'balanced': string,
+  'none': string;
+  'balanced': string;
   'screen': {
-    'introduction': string,
-    'game': string
-  },
-  'separateWater': string,
-  'hideWhy': string,
-  'showAnswer': string,
-  'tryAgain': string,
-  'notBalanced': string,
+    'introduction': string;
+    'game': string;
+  };
+  'separateWater': string;
+  'hideWhy': string;
+  'showAnswer': string;
+  'tryAgain': string;
+  'notBalanced': string;
   'balancing-chemical-equations': {
-    'title': string
-  },
-  'combustMethane': string,
-  'next': string,
-  'check': string,
-  'makeAmmonia': string,
-  'showWhy': string,
-  'notSimplified': string,
-  'tools': string,
-  'reactants': string,
-  'products': string,
-  'chooseYourLevel': string,
-  'pattern_0level': string,
-  'pattern_0points': string
+    'title': string;
+  };
+  'combustMethane': string;
+  'next': string;
+  'check': string;
+  'makeAmmonia': string;
+  'showWhy': string;
+  'notSimplified': string;
+  'tools': string;
+  'reactants': string;
+  'products': string;
+  'chooseYourLevel': string;
+  'pattern_0level': string;
+  'pattern_0points': string;
 };
 
 const balancingChemicalEquationsStrings = getStringModule( 'BALANCING_CHEMICAL_EQUATIONS' ) as StringsType;
