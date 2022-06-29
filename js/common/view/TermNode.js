a5726b9a321982bf9d64f03b53ccb258eff1b2a2
phet-dev <phetadmin@gmail.com>
2022-06-29 03:20:46 -0600

    update copyright dates from daily grunt work

--- js/common/view/TermNode.js
+++ js/common/view/TermNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2021, University of Colorado Boulder
+// Copyright 2014-2022, University of Colorado Boulder
 
 /**
  * A term in the equation, includes the coefficient and symbol.
