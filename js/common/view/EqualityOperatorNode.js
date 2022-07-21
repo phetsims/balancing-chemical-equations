4e1f52fb306731108833b15cdefb5e82d5e1305f
phet-dev <phetadmin@gmail.com>
2022-07-21 04:03:36 -0600

    update copyright dates from daily grunt work

--- js/common/view/EqualityOperatorNode.js
+++ js/common/view/EqualityOperatorNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2021, University of Colorado Boulder
+// Copyright 2014-2022, University of Colorado Boulder
 
 /**
  * Equality operator between 2 sides of equation: equals (balanced) or not equals (not balanced).
