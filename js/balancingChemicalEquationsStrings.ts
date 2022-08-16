25ca82c20dd8b7a55544e4072071714902445f5f
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:32 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/balancingChemicalEquationsStrings.ts
+++ js/balancingChemicalEquationsStrings.ts
@@ -5,35 +5,58 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import balancingChemicalEquations from './balancingChemicalEquations.js';
 
 type StringsType = {
   'none': string;
+  'noneProperty': TReadOnlyProperty<string>;
   'balanced': string;
+  'balancedProperty': TReadOnlyProperty<string>;
   'screen': {
     'introduction': string;
+    'introductionProperty': TReadOnlyProperty<string>;
     'game': string;
+    'gameProperty': TReadOnlyProperty<string>;
   };
   'separateWater': string;
+  'separateWaterProperty': TReadOnlyProperty<string>;
   'hideWhy': string;
+  'hideWhyProperty': TReadOnlyProperty<string>;
   'showAnswer': string;
+  'showAnswerProperty': TReadOnlyProperty<string>;
   'tryAgain': string;
+  'tryAgainProperty': TReadOnlyProperty<string>;
   'notBalanced': string;
+  'notBalancedProperty': TReadOnlyProperty<string>;
   'balancing-chemical-equations': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'combustMethane': string;
+  'combustMethaneProperty': TReadOnlyProperty<string>;
   'next': string;
+  'nextProperty': TReadOnlyProperty<string>;
   'check': string;
+  'checkProperty': TReadOnlyProperty<string>;
   'makeAmmonia': string;
+  'makeAmmoniaProperty': TReadOnlyProperty<string>;
   'showWhy': string;
+  'showWhyProperty': TReadOnlyProperty<string>;
   'notSimplified': string;
+  'notSimplifiedProperty': TReadOnlyProperty<string>;
   'tools': string;
+  'toolsProperty': TReadOnlyProperty<string>;
   'reactants': string;
+  'reactantsProperty': TReadOnlyProperty<string>;
   'products': string;
+  'productsProperty': TReadOnlyProperty<string>;
   'chooseYourLevel': string;
+  'chooseYourLevelProperty': TReadOnlyProperty<string>;
   'pattern_0level': string;
+  'pattern_0levelProperty': TReadOnlyProperty<string>;
   'pattern_0points': string;
+  'pattern_0pointsProperty': TReadOnlyProperty<string>;
 };
 
 const balancingChemicalEquationsStrings = getStringModule( 'BALANCING_CHEMICAL_EQUATIONS' ) as StringsType;
