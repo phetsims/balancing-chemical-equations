b0e4ed50579c3884f0588af22afdb750d954bf88
pixelzoom <cmalley@pixelzoom.com>
2023-01-03 13:05:47 -0700

    convert to TypeScript

--- js/balancing-chemical-equations-main.ts
+++ js/balancing-chemical-equations-main.ts
@@ -1,6 +1,5 @@
 // Copyright 2014-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * Main entry point for the 'Balancing Chemical Equations' sim.
  */
@@ -11,21 +10,23 @@ import BalancingChemicalEquationsStrings from './BalancingChemicalEquationsStrin
 import GameScreen from './game/GameScreen.js';
 import IntroductionScreen from './introduction/IntroductionScreen.js';
 
-const simOptions = {
-  credits: {
-    leadDesign: 'Kelly Lancaster (Java), Yuen-ying Carpenter (HTML5)',
-    softwareDevelopment: 'Chris Malley (PixelZoom, Inc.)',
-    team: 'Julia Chamberlain, Patricia Loeblein, Emily B. Moore, Robert Parson, Ariel Paul, Kathy Perkins, Amy Rouinfar',
-    qualityAssurance: 'Steele Dalton, Bryce Griebenow, Elise Morgan, Liam Mulhall, Oliver Orejola, Laura Rae, Benjamin Roberts, Jacob Romero, Kathryn Woessner, Kelly Wurtz, Bryan Yoelin',
-    thanks: '\u2022 Conversion of this simulation to HTML5 was funded in part by the American Association of Chemistry Teachers (AACT).<br>' +
-            '\u2022 Thanks to Mobile Learner Labs for working with the PhET development team to convert this simulation to HTML5.'
-  }
-};
-
 simLauncher.launch( () => {
+
   const screens = [
     new IntroductionScreen(),
     new GameScreen()
   ];
-  new Sim( BalancingChemicalEquationsStrings[ 'balancing-chemical-equations' ].titleStringProperty, screens, simOptions ).start();
+
+  const sim = new Sim( BalancingChemicalEquationsStrings[ 'balancing-chemical-equations' ].titleStringProperty, screens, {
+    credits: {
+      leadDesign: 'Kelly Lancaster (Java), Yuen-ying Carpenter (HTML5)',
+      softwareDevelopment: 'Chris Malley (PixelZoom, Inc.)',
+      team: 'Julia Chamberlain, Patricia Loeblein, Emily B. Moore, Robert Parson, Ariel Paul, Kathy Perkins, Amy Rouinfar',
+      qualityAssurance: 'Steele Dalton, Bryce Griebenow, Elise Morgan, Liam Mulhall, Oliver Orejola, Laura Rae, Benjamin Roberts, Jacob Romero, Kathryn Woessner, Kelly Wurtz, Bryan Yoelin',
+      thanks: '\u2022 Conversion of this simulation to HTML5 was funded in part by the American Association of Chemistry Teachers (AACT).<br>' +
+              '\u2022 Thanks to Mobile Learner Labs for working with the PhET development team to convert this simulation to HTML5.'
+    }
+  } );
+
+  sim.start();
 } );
