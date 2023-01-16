4c0ae9f9ef82538afeac0ef0e1bd39d3cc8f4c4d
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-01-16 11:06:17 -0700

    opt out of todo-should-have-issue in package.json of the repo, https://github.com/phetsims/chipper/issues/1372

--- js/game/view/LevelSelectionNode.js
+++ js/game/view/LevelSelectionNode.js
@@ -71,7 +71,7 @@ export default class LevelSelectionNode extends Node {
       },
       flowBoxOptions: {
         spacing: 50,
-        resize: false, //TODO
+        resize: false, //TODO, https://github.com/phetsims/vegas/issues/108
         center: layoutBounds.center
       }
     } );
