94ecc9bf3b84179d769abe20af571c1973a0c732
pixelzoom <cmalley@pixelzoom.com>
2023-02-22 14:27:00 -0700

    convert to TypeScript

--- js/common/model/Molecule.ts
+++ js/common/model/Molecule.ts
@@ -1,52 +1,44 @@
 // Copyright 2014-2023, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * Model of a molecule.
  *
  * @author Vasily Shakhov (mlearner.com)
+ * @author Chris Malley (PixelZoom, Inc.)
  */
 
 import Atom from '../../../../nitroglycerin/js/Atom.js';
+import MoleculeNode, { MoleculeNodeOptions } from '../../../../nitroglycerin/js/nodes/MoleculeNode.js';
+import Element from '../../../../nitroglycerin/js/Element.js';
 import balancingChemicalEquations from '../../balancingChemicalEquations.js';
 
 export default class Molecule {
 
-  /**
-   * @param {NITROGLYCERIN.node} nodeConstructor constructor of molecule from NITROGLYCERIN
-   * @param {string} symbolText html string
-   * @param {NITROGLYCERIN.Element[]} elements
-   */
-  constructor( nodeConstructor, symbolText, elements ) {
+  private readonly MoleculeNodeConstructor: new ( options?: MoleculeNodeOptions ) => MoleculeNode;
+  public readonly symbol: string;
+  public readonly atoms: Atom[];
 
-    // @private
-    this.nodeConstructor = nodeConstructor;
+  public constructor( MoleculeNodeConstructor: new ( options?: MoleculeNodeOptions ) => MoleculeNode, symbol: string, elements: Element[] ) {
 
-    // @public
-    this.symbol = symbolText;
+    this.MoleculeNodeConstructor = MoleculeNodeConstructor;
+    this.symbol = symbol;
     this.atoms = [];
 
     elements.forEach( element => this.atoms.push( new Atom( element ) ) );
   }
 
   /**
-   * Any molecule with more than 5 atoms is considered "big".
-   * This affects degree of difficulty in the Game.
-   * @returns {boolean}
-   * @public
+   * Any molecule with more than 5 atoms is considered "big". This affects degree of difficulty in the Game.
    */
-  isBig() {
+  public isBig(): boolean {
     return this.atoms.length > 5;
   }
 
   /**
    * Creates the MoleculeNode that corresponds to this Molecule.
-   * @param [options]
-   * @returns {MoleculeNode}
-   * @public
    */
-  createNode( options ) {
-    return new this.nodeConstructor( options );
+  public createNode( options?: MoleculeNodeOptions ): MoleculeNode {
+    return new this.MoleculeNodeConstructor( options );
   }
 }
 
--- js/common/model/MoleculeFactory.ts
+++ js/common/model/MoleculeFactory.ts
@@ -1,6 +1,5 @@
 // Copyright 2014-2021, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * Factory for creating molecule nodes.
  *
@@ -45,145 +44,50 @@ import SO3Node from '../../../../nitroglycerin/js/nodes/SO3Node.js';
 import balancingChemicalEquations from '../../balancingChemicalEquations.js';
 import Molecule from './Molecule.js';
 
-// modules - molecules
+const C = Element.C;
+const Cl = Element.Cl;
+const F = Element.F;
+const H = Element.H;
+const N = Element.N;
+const O = Element.O;
+const P = Element.P;
+const S = Element.S;
 
 const MoleculeFactory = {
-
-  C: function() {
-    return new Molecule( CNode, 'C', [ Element.C ] );
-  },
-
-  Cl2: function() {
-    return new Molecule( Cl2Node, 'Cl<sub>2</sub>', [ Element.Cl, Element.Cl ] );
-  },
-
-  C2H2: function() {
-    return new Molecule( C2H2Node, 'C<sub>2</sub>H<sub>2</sub>', [ Element.C, Element.C, Element.H, Element.H ] );
-  },
-
-  C2H4: function() {
-    return new Molecule( C2H4Node, 'C<sub>2</sub>H<sub>4</sub>', [ Element.C, Element.C, Element.H, Element.H, Element.H, Element.H ] );
-  },
-
-  C2H5Cl: function() {
-    return new Molecule( C2H5ClNode, 'C<sub>2</sub>H<sub>5</sub>Cl', [ Element.C, Element.C, Element.H, Element.H, Element.H, Element.H, Element.H, Element.Cl ] );
-  },
-
-  C2H5OH: function() {
-    return new Molecule( C2H5OHNode, 'C<sub>2</sub>H<sub>5</sub>OH', [ Element.C, Element.C, Element.H, Element.H, Element.H, Element.H, Element.H, Element.O, Element.H ] );
-  },
-
-  C2H6: function() {
-    return new Molecule( C2H6Node, 'C<sub>2</sub>H<sub>6</sub>', [ Element.C, Element.C, Element.H, Element.H, Element.H, Element.H, Element.H, Element.H ] );
-  },
-
-  CH2O: function() {
-    return new Molecule( CH2ONode, 'CH<sub>2</sub>O', [ Element.C, Element.H, Element.H, Element.O ] );
-  },
-
-  CH3OH: function() {
-    return new Molecule( CH3OHNode, 'CH<sub>3</sub>OH', [ Element.C, Element.H, Element.H, Element.H, Element.O, Element.H ] );
-  },
-
-  CH4: function() {
-    return new Molecule( CH4Node, 'CH<sub>4</sub>', [ Element.C, Element.H, Element.H, Element.H, Element.H ] );
-  },
-
-  CO: function() {
-    return new Molecule( CONode, 'CO', [ Element.C, Element.O ] );
-  },
-
-  CO2: function() {
-    return new Molecule( CO2Node, 'CO<sub>2</sub>', [ Element.C, Element.O, Element.O ] );
-  },
-
-  CS2: function() {
-    return new Molecule( CS2Node, 'CS<sub>2</sub>', [ Element.C, Element.S, Element.S ] );
-  },
-
-  F2: function() {
-    return new Molecule( F2Node, 'F<sub>2</sub>', [ Element.F, Element.F ] );
-  },
-
-  H2: function() {
-    return new Molecule( H2Node, 'H<sub>2</sub>', [ Element.H, Element.H ] );
-  },
-
-  H2O: function() {
-    return new Molecule( H2ONode, 'H<sub>2</sub>O', [ Element.H, Element.H, Element.O ] );
-  },
-
-  H2S: function() {
-    return new Molecule( H2SNode, 'H<sub>2</sub>S', [ Element.H, Element.H, Element.S ] );
-  },
-
-  HF: function() {
-    return new Molecule( HFNode, 'HF', [ Element.H, Element.F ] );
-  },
-
-  HCl: function() {
-    return new Molecule( HClNode, 'HCl', [ Element.H, Element.Cl ] );
-  },
-
-  N2: function() {
-    return new Molecule( N2Node, 'N<sub>2</sub>', [ Element.N, Element.N ] );
-  },
-
-  N2O: function() {
-    return new Molecule( N2ONode, 'N<sub>2</sub>O', [ Element.N, Element.N, Element.O ] );
-  },
-
-  NH3: function() {
-    return new Molecule( NH3Node, 'NH<sub>3</sub>', [ Element.N, Element.H, Element.H, Element.H ] );
-  },
-
-  NO: function() {
-    return new Molecule( NONode, 'NO', [ Element.N, Element.O ] );
-  },
-
-  NO2: function() {
-    return new Molecule( NO2Node, 'NO<sub>2</sub>', [ Element.N, Element.O, Element.O ] );
-  },
-
-  O2: function() {
-    return new Molecule( O2Node, 'O<sub>2</sub>', [ Element.O, Element.O ] );
-  },
-
-  OF2: function() {
-    return new Molecule( OF2Node, 'OF<sub>2</sub>', [ Element.O, Element.F, Element.F ] );
-  },
-
-  P4: function() {
-    return new Molecule( P4Node, 'P<sub>4</sub>', [ Element.P, Element.P, Element.P, Element.P ] );
-  },
-
-  PH3: function() {
-    return new Molecule( PH3Node, 'PH<sub>3</sub>', [ Element.P, Element.H, Element.H, Element.H ] );
-  },
-
-  PCl3: function() {
-    return new Molecule( PCl3Node, 'PCl<sub>3</sub>', [ Element.P, Element.Cl, Element.Cl, Element.Cl ] );
-  },
-
-  PCl5: function() {
-    return new Molecule( PCl5Node, 'PCl<sub>5</sub>', [ Element.P, Element.Cl, Element.Cl, Element.Cl, Element.Cl, Element.Cl ] );
-  },
-
-  PF3: function() {
-    return new Molecule( PF3Node, 'PF<sub>3</sub>', [ Element.P, Element.F, Element.F, Element.F ] );
-  },
-
-  S: function() {
-    return new Molecule( SNode, 'S', [ Element.S ] );
-  },
-
-  SO3: function() {
-    return new Molecule( SO3Node, 'SO<sub>3</sub>', [ Element.S, Element.O, Element.O, Element.O ] );
-  },
-
-  SO2: function() {
-    return new Molecule( SO2Node, 'SO<sub>2</sub>', [ Element.S, Element.O, Element.O ] );
-  }
+  C: (): Molecule => new Molecule( CNode, 'C', [ C ] ),
+  Cl2: (): Molecule => new Molecule( Cl2Node, 'Cl<sub>2</sub>', [ Cl, Cl ] ),
+  C2H2: (): Molecule => new Molecule( C2H2Node, 'C<sub>2</sub>H<sub>2</sub>', [ C, C, H, H ] ),
+  C2H4: (): Molecule => new Molecule( C2H4Node, 'C<sub>2</sub>H<sub>4</sub>', [ C, C, H, H, H, H ] ),
+  C2H5Cl: (): Molecule => new Molecule( C2H5ClNode, 'C<sub>2</sub>H<sub>5</sub>Cl', [ C, C, H, H, H, H, H, Cl ] ),
+  C2H5OH: (): Molecule => new Molecule( C2H5OHNode, 'C<sub>2</sub>H<sub>5</sub>OH', [ C, C, H, H, H, H, H, O, H ] ),
+  C2H6: (): Molecule => new Molecule( C2H6Node, 'C<sub>2</sub>H<sub>6</sub>', [ C, C, H, H, H, H, H, H ] ),
+  CH2O: (): Molecule => new Molecule( CH2ONode, 'CH<sub>2</sub>O', [ C, H, H, O ] ),
+  CH3OH: (): Molecule => new Molecule( CH3OHNode, 'CH<sub>3</sub>OH', [ C, H, H, H, O, H ] ),
+  CH4: (): Molecule => new Molecule( CH4Node, 'CH<sub>4</sub>', [ C, H, H, H, H ] ),
+  CO: (): Molecule => new Molecule( CONode, 'CO', [ C, O ] ),
+  CO2: (): Molecule => new Molecule( CO2Node, 'CO<sub>2</sub>', [ C, O, O ] ),
+  CS2: (): Molecule => new Molecule( CS2Node, 'CS<sub>2</sub>', [ C, S, S ] ),
+  F2: (): Molecule => new Molecule( F2Node, 'F<sub>2</sub>', [ F, F ] ),
+  H2: (): Molecule => new Molecule( H2Node, 'H<sub>2</sub>', [ H, H ] ),
+  H2O: (): Molecule => new Molecule( H2ONode, 'H<sub>2</sub>O', [ H, H, O ] ),
+  H2S: (): Molecule => new Molecule( H2SNode, 'H<sub>2</sub>S', [ H, H, S ] ),
+  HF: (): Molecule => new Molecule( HFNode, 'HF', [ H, F ] ),
+  HCl: (): Molecule => new Molecule( HClNode, 'HCl', [ H, Cl ] ),
+  N2: (): Molecule => new Molecule( N2Node, 'N<sub>2</sub>', [ N, N ] ),
+  N2O: (): Molecule => new Molecule( N2ONode, 'N<sub>2</sub>O', [ N, N, O ] ),
+  NH3: (): Molecule => new Molecule( NH3Node, 'NH<sub>3</sub>', [ N, H, H, H ] ),
+  NO: (): Molecule => new Molecule( NONode, 'NO', [ N, O ] ),
+  NO2: (): Molecule => new Molecule( NO2Node, 'NO<sub>2</sub>', [ N, O, O ] ),
+  O2: (): Molecule => new Molecule( O2Node, 'O<sub>2</sub>', [ O, O ] ),
+  OF2: (): Molecule => new Molecule( OF2Node, 'OF<sub>2</sub>', [ O, F, F ] ),
+  P4: (): Molecule => new Molecule( P4Node, 'P<sub>4</sub>', [ P, P, P, P ] ),
+  PH3: (): Molecule => new Molecule( PH3Node, 'PH<sub>3</sub>', [ P, H, H, H ] ),
+  PCl3: (): Molecule => new Molecule( PCl3Node, 'PCl<sub>3</sub>', [ P, Cl, Cl, Cl ] ),
+  PCl5: (): Molecule => new Molecule( PCl5Node, 'PCl<sub>5</sub>', [ P, Cl, Cl, Cl, Cl, Cl ] ),
+  PF3: (): Molecule => new Molecule( PF3Node, 'PF<sub>3</sub>', [ P, F, F, F ] ),
+  S: (): Molecule => new Molecule( SNode, 'S', [ S ] ),
+  SO3: (): Molecule => new Molecule( SO3Node, 'SO<sub>3</sub>', [ S, O, O, O ] ),
+  SO2: (): Molecule => new Molecule( SO2Node, 'SO<sub>2</sub>', [ S, O, O ] )
 };
 
 balancingChemicalEquations.register( 'MoleculeFactory', MoleculeFactory );
