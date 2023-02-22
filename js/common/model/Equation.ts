a74a019dc32b3f382e10510778fb7783225e6896
pixelzoom <cmalley@pixelzoom.com>
2023-02-22 15:38:19 -0700

    convert to TypeScript

--- js/common/model/Equation.ts
+++ js/common/model/Equation.ts
@@ -1,8 +1,7 @@
 // Copyright 2014-2023, University of Colorado Boulder
 
-// @ts-nocheck
 /**
- * Base type for all chemical equations.
+ * Equation is the base class for all chemical equations.
  * A chemical equation has 2 sets of terms, reactants and products.
  * During the chemical reaction represented by the equation, reactants are transformed into products.
  * <p/>
@@ -16,43 +15,55 @@
 
 import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
 import NumberProperty from '../../../../axon/js/NumberProperty.js';
+import Property from '../../../../axon/js/Property.js';
 import balancingChemicalEquations from '../../balancingChemicalEquations.js';
 import AtomCount from './AtomCount.js';
+import EquationTerm from './EquationTerm.js';
 
 export default class Equation {
 
-  /**
-   * @param {EquationTerm[]} reactants terms on the left side of the equation
-   * @param {EquationTerm[]} products terms on the right side of the equation
-   */
-  constructor( reactants, products ) {
+  // terms on the left side of the equation
+  public readonly reactants: EquationTerm[];
 
-    // @public
+  // terms on the right side of the equation
+  public readonly products: EquationTerm[];
+
+  // whether the equation is balanced
+  public readonly balancedProperty: Property<boolean>;
+
+  // a sum of all coefficients, so we know when the sum is non-zero
+  public readonly coefficientsSumProperty: Property<number>;
+
+  // true = balanced with the lowest possible coefficients
+  public balancedAndSimplified: boolean;
+
+  /**
+   * @param reactants terms on the left side of the equation
+   * @param products terms on the right side of the equation
+   */
+  protected constructor( reactants: EquationTerm[], products: EquationTerm[] ) {
+
     this.reactants = reactants;
     this.products = products;
-
-    // @public
     this.balancedProperty = new BooleanProperty( false );
     this.coefficientsSumProperty = new NumberProperty( 0, { numberType: 'Integer' } );
-
-    this.balancedAndSimplified = false; // @public balanced with the lowest possible coefficients
+    this.balancedAndSimplified = false;
 
     this.addCoefficientsObserver( this.updateBalanced.bind( this ) );
 
     // keep a sum of all coefficients, so we know when the sum is non-zero
     this.addCoefficientsObserver( () => {
       let coefficientsSum = 0;
-      const addCoefficients = equationTerm => {
-        coefficientsSum += equationTerm.userCoefficientProperty.get();
+      const addCoefficients = ( equationTerm: EquationTerm ) => {
+        coefficientsSum += equationTerm.userCoefficientProperty.value;
       };
-      this.reactants.forEach( addCoefficients );
-      this.products.forEach( addCoefficients );
-      this.coefficientsSumProperty.set( coefficientsSum );
+      this.reactants.forEach( reactant => addCoefficients( reactant ) );
+      this.products.forEach( product => addCoefficients( product ) );
+      this.coefficientsSumProperty.value = coefficientsSum;
     } );
   }
 
-  // @public
-  reset() {
+  public reset(): void {
 
     this.balancedProperty.reset();
     this.coefficientsSumProperty.reset();
@@ -62,35 +73,32 @@ export default class Equation {
   }
 
   /**
-   * An equation is balanced if all of its terms have a coefficient that is the
-   * same integer multiple of the term's balanced coefficient.  If the integer
-   * multiple is 1, then the term is "balanced and simplified" (balanced with
-   * lowest possible coefficients).
-   * @private
+   * An equation is balanced if all of its terms have a coefficient that is the same integer multiple of the term's
+   * balanced coefficient. If the integer multiple is 1, then the term is "balanced and simplified" - balanced with
+   * the lowest possible coefficients.
    */
-  updateBalanced() {
+  private updateBalanced(): void {
 
     // Get integer multiplier from the first reactant term.
-    const multiplier = this.reactants[ 0 ].userCoefficientProperty.get() / this.reactants[ 0 ].balancedCoefficient;
+    const multiplier = this.reactants[ 0 ].userCoefficientProperty.value / this.reactants[ 0 ].balancedCoefficient;
     let balanced = ( multiplier > 0 );
 
     // Check each term to see if the actual coefficient is the same integer multiple of the balanced coefficient.
     this.reactants.forEach( reactant => {
-      balanced = balanced && ( reactant.userCoefficientProperty.get() === multiplier * reactant.balancedCoefficient );
+      balanced = balanced && ( reactant.userCoefficientProperty.value === multiplier * reactant.balancedCoefficient );
     } );
     this.products.forEach( product => {
-      balanced = balanced && ( product.userCoefficientProperty.get() === multiplier * product.balancedCoefficient );
+      balanced = balanced && ( product.userCoefficientProperty.value === multiplier * product.balancedCoefficient );
     } );
 
     this.balancedAndSimplified = balanced && ( multiplier === 1 ); // set the more specific value first
-    this.balancedProperty.set( balanced );
+    this.balancedProperty.value = balanced;
   }
 
   /**
    * Convenience method for adding an observer to all coefficients.
-   * @public
    */
-  addCoefficientsObserver( observer ) {
+  public addCoefficientsObserver( observer: () => void ): void {
     this.reactants.forEach( reactant => reactant.userCoefficientProperty.lazyLink( observer ) );
     this.products.forEach( product => product.userCoefficientProperty.lazyLink( observer ) );
     observer();
@@ -98,60 +106,45 @@ export default class Equation {
 
   /**
    * Convenience method for removing an observer from all coefficients.
-   * @public
    */
-  removeCoefficientsObserver( observer ) {
+  public removeCoefficientsObserver( observer: () => void ): void {
     this.reactants.forEach( reactant => reactant.userCoefficientProperty.unlink( observer ) );
     this.products.forEach( product => product.userCoefficientProperty.unlink( observer ) );
   }
 
   /**
    * Returns a count of each type of atom, based on the user coefficients.
-   * See AtomCount.countAtoms for details.
-   * @returns {AtomCount[]}
-   * @public
    */
-  getAtomCounts() {
+  public getAtomCounts(): AtomCount[] {
     return AtomCount.countAtoms( this );
   }
 
   /**
-   * Does this equation contain at least one "big" molecule?
-   * This affects degree of difficulty in the Game.
-   * @returns {boolean}
-   * @public
+   * Does this equation contain at least one "big" molecule? This affects degree of difficulty in the Game.
    */
-  hasBigMolecule() {
-    this.reactants.forEach( reactant => { // eslint-disable-line consistent-return
-      if ( reactant.molecule.isBig() ) {
-        return true;
-      }
-    } );
-    this.products.forEach( product => { // eslint-disable-line consistent-return
-      if ( product.molecule.isBig() ) {
-        return true;
-      }
-    } );
-    return false;
+  public hasBigMolecule(): boolean {
+    return !!_.find( this.reactants, reactant => reactant.molecule.isBig() ) ||
+           !!_.find( this.products, product => product.molecule.isBig() );
   }
 
   /**
    * Balances the equation by copying the balanced coefficient value to
    * the user coefficient value for each term in the equation.
-   * @public
    */
-  balance() {
-    this.reactants.forEach( term => term.userCoefficientProperty.set( term.balancedCoefficient ) );
-    this.products.forEach( term => term.userCoefficientProperty.set( term.balancedCoefficient ) );
+  public balance(): void {
+    this.reactants.forEach( term => {
+      term.userCoefficientProperty.value = term.balancedCoefficient;
+    } );
+    this.products.forEach( term => {
+      term.userCoefficientProperty.value = term.balancedCoefficient;
+    } );
   }
 
   /**
    * Gets a string that shows just the coefficients of the equations.
    * This is used to show game answers when running in 'dev' mode.
-   * @returns {string}
-   * @public
    */
-  getCoefficientsString() {
+  public getCoefficientsString(): string {
     let string = '';
     for ( let i = 0; i < this.reactants.length; i++ ) {
       string += this.reactants[ i ].balancedCoefficient;
@@ -166,11 +159,9 @@ export default class Equation {
   }
 
   /**
-   * String value of an equation, shows balanced coefficients, for debugging.
-   * @returns {string}
-   * @public
+   * String value of an equation, shows balanced coefficients, for debugging. Do not rely on this format!
    */
-  toString() {
+  public toString(): string {
     let string = '';
 
     // reactants
