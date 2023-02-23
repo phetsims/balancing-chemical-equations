4d8c541132bab107cd43b7b9f9bd6456fda7a1fc
pixelzoom <cmalley@pixelzoom.com>
2023-02-22 18:53:33 -0700

    convert to TypeScript

--- js/common/view/TermNode.ts
+++ js/common/view/TermNode.ts
@@ -1,6 +1,5 @@
 // Copyright 2014-2023, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * A term in the equation, includes the coefficient and symbol.
  * The coefficient may or may not be editable.
@@ -10,28 +9,36 @@
  */
 
 import Property from '../../../../axon/js/Property.js';
-import merge from '../../../../phet-core/js/merge.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
-import { Node, RichText } from '../../../../scenery/js/imports.js';
+import Range from '../../../../dot/js/Range.js';
+import { Node, NodeOptions, RichText } from '../../../../scenery/js/imports.js';
 import NumberPicker from '../../../../sun/js/NumberPicker.js';
 import balancingChemicalEquations from '../../balancingChemicalEquations.js';
+import EquationTerm from '../model/EquationTerm.js';
+import optionize from '../../../../phet-core/js/optionize.js';
+
+type SelfOptions = {
+  fontSize?: number;
+  xSpacing?: number;
+};
+
+type TermNodeOptions = SelfOptions;
 
 export default class TermNode extends Node {
 
-  /**
-   * @param {DOT.Range} coefficientRange
-   * @param {EquationTerm} term
-   * @param {Object} [options]
-   */
-  constructor( coefficientRange, term, options ) {
+  private readonly coefficientPicker: NumberPicker;
 
-    options = merge( {
+  public constructor( coefficientRange: Range, term: EquationTerm, providedOptions?: TermNodeOptions ) {
+
+    const options = optionize<TermNodeOptions, SelfOptions, NodeOptions>()( {
+
+      // SelfOptions
       fontSize: 32,
       xSpacing: 4
-    }, options );
+    }, providedOptions );
 
     // coefficient picker
-    const coefficientNode = new NumberPicker( term.userCoefficientProperty, new Property( coefficientRange ), {
+    const coefficientPicker = new NumberPicker( term.userCoefficientProperty, new Property( coefficientRange ), {
       color: 'rgb(50,50,50)',
       pressedColor: 'black',
       xMargin: 8,
@@ -45,30 +52,26 @@ export default class TermNode extends Node {
     // symbol, non-subscript part of the symbol is vertically centered on the picker
     const richTextOptions = { font: new PhetFont( options.fontSize ) };
     const symbolNode = new RichText( term.molecule.symbol, richTextOptions );
-    symbolNode.left = coefficientNode.right + options.xSpacing;
-    symbolNode.centerY = coefficientNode.centerY + ( symbolNode.height - new RichText( 'H', richTextOptions ).height ) / 2;
+    symbolNode.left = coefficientPicker.right + options.xSpacing;
+    symbolNode.centerY = coefficientPicker.centerY + ( symbolNode.height - new RichText( 'H', richTextOptions ).height ) / 2;
 
-    options.children = [ coefficientNode, symbolNode ];
+    options.children = [ coefficientPicker, symbolNode ];
     super( options );
 
-    // @private
-    this.coefficientNode = coefficientNode;
+    this.coefficientPicker = coefficientPicker;
   }
 
-  // @public
-  dispose() {
-    this.coefficientNode.dispose();
+  public override dispose(): void {
+    this.coefficientPicker.dispose();
     super.dispose();
   }
 
   /**
    * Sets whether the term's coefficient is editable, by showing/hiding the arrows on the NumberPicker.
-   * @param editable
-   * @public
    */
-  setCoefficientEditable( editable ) {
+  public setCoefficientEditable( editable: boolean ): void {
     this.pickable = editable;
-    this.coefficientNode.setArrowsVisible( editable );
+    this.coefficientPicker.setArrowsVisible( editable );
   }
 }
 
