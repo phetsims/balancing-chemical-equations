ee8790dfc31a135012d7fd312c5000fc1e1e7e56
Chris Malley <cmalley@pixelzoom.com>
2023-02-23 12:18:14 -0700

    convert to TypeScript

--- js/common/view/BarChartsNode.ts
+++ js/common/view/BarChartsNode.ts
@@ -1,6 +1,5 @@
 // Copyright 2014-2023, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * Visual representation of an equation as a pair of bar charts, for left and right side of equation.
  * An indicator between the charts (equals or not equals) indicates whether they are balanced.
@@ -14,36 +13,57 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import Element from '../../../../nitroglycerin/js/Element.js';
 import NumberProperty from '../../../../axon/js/NumberProperty.js';
+import Property from '../../../../axon/js/Property.js';
+import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
-import merge from '../../../../phet-core/js/merge.js';
-import { Node } from '../../../../scenery/js/imports.js';
+import { Node, NodeOptions, NodeTranslationOptions } from '../../../../scenery/js/imports.js';
 import balancingChemicalEquations from '../../balancingChemicalEquations.js';
+import Equation from '../model/Equation.js';
 import BarNode from './BarNode.js';
 import EqualityOperatorNode from './EqualityOperatorNode.js';
+import HorizontalAligner from './HorizontalAligner.js';
+import AtomCount from '../model/AtomCount.js';
+import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
+
+type SelfOptions = EmptySelfOptions;
+
+type BarChartsNodeOptions = SelfOptions & NodeTranslationOptions;
 
 export default class BarChartsNode extends Node {
 
-  /**
-   * @param {Property} equationProperty the equation that the scales are representing
-   * @param {HorizontalAligner} aligner provides layout information to ensure horizontal alignment with other user-interface elements
-   * @param {Object} [options]
-   */
-  constructor( equationProperty, aligner, options ) {
+  private readonly equationProperty: TReadOnlyProperty<Equation>;
+  private readonly aligner: HorizontalAligner;
 
-    options = merge( {}, options );
+  // maps Element to count for that Element
+  private readonly reactantCountProperties: Map<Element, Property<number>>;
+  private readonly productCountProperties: Map<Element, Property<number>>;
+
+  private readonly reactantBarsParent: Node;
+  private readonly productBarsParent: Node;
+
+  /**
+   * @param equationProperty - the equation that the scales are representing
+   * @param aligner - provides layout information to ensure horizontal alignment with other user-interface elements
+   * @param [providedOptions]
+   */
+  public constructor( equationProperty: TReadOnlyProperty<Equation>, aligner: HorizontalAligner,
+                      providedOptions?: BarChartsNodeOptions ) {
+
+    const options = optionize<BarChartsNodeOptions, SelfOptions, NodeOptions>()( {}, providedOptions );
 
     super();
 
-    this.equationProperty = equationProperty; // @private
-    this.aligner = aligner; // @private
-    this.reactantCountProperties = {}; // @private maps {string} Element.symbol to {Property.<number>} count of the element
-    this.productCountProperties = {}; // @private maps {string} Element.symbol to {Property.<number>} counts of the element
+    this.equationProperty = equationProperty;
+    this.aligner = aligner;
 
-    this.reactantBarsParent = new Node(); // @private
-    this.productBarsParent = new Node(); // @private
+    this.reactantCountProperties = new Map();
+    this.productCountProperties = new Map();
 
-    // @private
+    this.reactantBarsParent = new Node();
+    this.productBarsParent = new Node();
+
     const equalityOperatorNode = new EqualityOperatorNode( equationProperty, {
       center: new Vector2( aligner.getScreenCenterX(), -40 )
     } );
@@ -68,20 +88,23 @@ export default class BarChartsNode extends Node {
 
   /**
    * Updates this node's entire geometry and layout
-   * @private
    */
-  updateNode() {
+  private updateNode(): void {
     if ( this.visible ) {
       const atomCounts = this.equationProperty.value.getAtomCounts();
 
-      this.reactantCountProperties = this.updateBars(
+      // reactant bars
+      this.updateBars(
+        this.reactantCountProperties,
         this.reactantBarsParent,
         atomCounts,
         atomCount => atomCount.reactantsCount,
         this.aligner.getReactantsBoxCenterX()
       );
 
-      this.productCountProperties = this.updateBars(
+      // product bars
+      this.updateBars(
+        this.productCountProperties,
         this.productBarsParent,
         atomCounts,
         atomCount => atomCount.productsCount,
@@ -94,56 +117,61 @@ export default class BarChartsNode extends Node {
 
   /**
    * Updates one set of bars (reactants or products).
-   * @param {Node} parentNode
-   * @param {AtomCount[]} atomCounts counts of each atom in the equation
-   * @param {function} getCount 1 parameter {AtomCount}, return {number}, either the reactants or products count
-   * @param {number} centerX centerX of the chart
-   * @private
+   * @param countProperties - map of Element to count for that Element
+   * @param parentNode
+   * @param atomCounts - counts of each atom in the equation
+   * @param getCount - gets the reactants or products count
+   * @param centerX - centerX of the chart
    */
-  updateBars( parentNode, atomCounts, getCount, centerX ) {
+  public updateBars( countProperties: Map<Element, Property<number>>, parentNode: Node, atomCounts: AtomCount[],
+                     getCount: ( atomCount: AtomCount ) => number, centerX: number ): void {
 
-    // dispose of children before calling removeAllChildren
-    parentNode.getChildren().forEach( child => {
-      if ( child.dispose() ) {
-        child.dispose();
+    if ( this.visible ) {
+
+      // Dispose and remove previous bars
+      parentNode.getChildren().forEach( child => child.dispose() );
+      parentNode.removeAllChildren();
+
+      // Clear the map.
+      countProperties.clear();
+
+      let barCenterX = 0;
+      for ( let i = 0; i < atomCounts.length; i++ ) {
+        const atomCount = atomCounts[ i ];
+
+        // populate the map
+        const countProperty = new NumberProperty( getCount( atomCount ), { numberType: 'Integer' } );
+        countProperties.set( atomCount.element, countProperty );
+
+        // add a bar node
+        const barNode = new BarNode( atomCount.element, countProperty, {
+          centerX: barCenterX,
+          bottom: 0
+        } );
+        parentNode.addChild( barNode );
+        barCenterX = barNode.centerX + 100;
       }
-    } );
 
-    parentNode.removeAllChildren(); // remove all the bar nodes
-
-    const countProperties = {}; // clear the map
-
-    let barCenterX = 0;
-    for ( let i = 0; i < atomCounts.length; i++ ) {
-      const atomCount = atomCounts[ i ];
-      // populate the map
-      const countProperty = new NumberProperty( getCount( atomCount ), { numberType: 'Integer' } );
-      countProperties[ atomCount.element.symbol ] = countProperty;
-      // add a bar node
-      const barNode = new BarNode( atomCount.element, countProperty, {
-        centerX: barCenterX,
-        bottom: 0
-      } );
-      parentNode.addChild( barNode );
-      barCenterX = barNode.centerX + 100;
+      parentNode.centerX = centerX;
     }
-
-    parentNode.centerX = centerX;
-
-    return countProperties;
   }
 
   /**
    * Updates the atom counts for each bar.
-   * @private
    */
-  updateCounts() {
+  private updateCounts(): void {
     if ( this.visible ) {
       const atomCounts = this.equationProperty.value.getAtomCounts();
       for ( let i = 0; i < atomCounts.length; i++ ) {
         const atomCount = atomCounts[ i ];
-        this.reactantCountProperties[ atomCount.element.symbol ].value = atomCount.reactantsCount;
-        this.productCountProperties[ atomCount.element.symbol ].value = atomCount.productsCount;
+
+        const reactantCountProperty = this.reactantCountProperties.get( atomCount.element )!;
+        assert && assert( reactantCountProperty );
+        reactantCountProperty.value = atomCount.reactantsCount;
+
+        const productCountProperty = this.productCountProperties.get( atomCount.element )!;
+        assert && assert( productCountProperty );
+        productCountProperty.value = atomCount.productsCount;
       }
     }
   }
