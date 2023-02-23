01deb049b7544f3925cd8fa566fad9c96eeddeb1
Chris Malley <cmalley@pixelzoom.com>
2023-02-23 12:20:43 -0700

    // @ts-nocheck

--- js/common/view/EquationNode.js
+++ js/common/view/EquationNode.js
@@ -1,5 +1,6 @@
 // Copyright 2014-2023, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * Displays a chemical equation.
  * Reactants are on the left-hand size, products are on the right-hand side.
