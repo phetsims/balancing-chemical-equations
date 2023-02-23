91e8553b8ddc5f4bd408c94b5a2cb6fbb4855818
Chris Malley <cmalley@pixelzoom.com>
2023-02-23 10:20:25 -0700

    // @ts-nocheck

--- js/common/view/RightArrowNode.js
+++ js/common/view/RightArrowNode.js
@@ -1,5 +1,6 @@
 // Copyright 2014-2023, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * An arrow that points left to right, from reactants to products.
  * Highlights when the equation is balanced.
