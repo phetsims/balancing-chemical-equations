f9f755dc54653d7e38f4f6adc3cc9a0d94cd3dfa
pixelzoom <cmalley@pixelzoom.com>
2023-09-26 16:32:38 -0600

    Change "Introduction" to "Intro" in class names, https://github.com/phetsims/balancing-chemical-equations/issues/151

--- js/intro/view/IntroViewProperties.ts
+++ js/intro/view/IntroViewProperties.ts
@@ -36,4 +36,4 @@ export default class IntroViewProperties {
   }
 }
 
-balancingChemicalEquations.register( 'IntroductionViewProperties', IntroViewProperties );
+balancingChemicalEquations.register( 'IntroViewProperties', IntroViewProperties );
