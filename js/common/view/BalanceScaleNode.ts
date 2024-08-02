b4fa846453a42ec3bfbc0d12834b7bd400af3d98
phet-dev <phetadmin@gmail.com>
2024-08-02 03:18:53 -0600

    update copyright dates from daily grunt work

--- js/common/view/BalanceScaleNode.ts
+++ js/common/view/BalanceScaleNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2023, University of Colorado Boulder
+// Copyright 2014-2024, University of Colorado Boulder
 
 /**
  * A balance scale, depicts the relationship between the atom count
