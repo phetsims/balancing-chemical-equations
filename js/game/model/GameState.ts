a21b9d875669ba3f90e6ed817a7661604ea8a17a
phet-dev <phetadmin@gmail.com>
2024-10-05 03:15:23 -0600

    update copyright dates from daily grunt work

--- js/game/model/GameState.ts
+++ js/game/model/GameState.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * States for the game. For lack of better names, the state names correspond to the main action that the user
