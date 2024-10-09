6045cb95a67ab3cac05e3cdd6344bcd137198a2b
phet-dev <phetadmin@gmail.com>
2024-10-09 03:26:32 -0600

    update copyright dates from daily grunt work

--- js/common/view/BarNode.ts
+++ js/common/view/BarNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2023, University of Colorado Boulder
+// Copyright 2014-2024, University of Colorado Boulder
 
 /**
  * A bar that displays some number of atoms for a specified atom.
