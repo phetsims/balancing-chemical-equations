1ebade138dfad32c8b1c3322073f7217e884cf38
phet-dev <phetadmin@gmail.com>
2025-03-08 03:20:21 -0700

    update copyright dates from daily grunt work

--- js/game/model/RandomStrategy.ts
+++ js/game/model/RandomStrategy.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2024, University of Colorado Boulder
+// Copyright 2020-2025, University of Colorado Boulder
 
 /**
  * Selects a random set from a pool of equation generators, with no duplicates. Base on the optional exclusionsMap,
