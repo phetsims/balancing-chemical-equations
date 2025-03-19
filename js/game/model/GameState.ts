95c0d188803f8c1b5250b8e9634a4ffdc4ac06ee
pixelzoom <cmalley@pixelzoom.com>
2025-03-19 14:02:27 -0600

    document game state transitions

--- js/game/model/GameState.ts
+++ js/game/model/GameState.ts
@@ -5,8 +5,17 @@
  * main action that the user can take in that state.  For example. the 'check' state is where the user can enter
  * coefficients and press the "Check" button to check their answer.
  *
+ * The possible state transitions are documented below.
+ *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-export const GameStateValues = [ 'levelSelection', 'check', 'tryAgain', 'showAnswer', 'next', 'levelCompleted' ] as const;
+export const GameStateValues = [
+  'levelSelection', // goes to 'check'
+  'check',          // goes to 'tryAgain' or 'showAnswer', depending on how many attempts have been made to solve the current challenge
+  'tryAgain',       // goes to 'check'
+  'showAnswer',     // goes to 'next'
+  'next',           // goes to 'check' if there are more challenges to play, or 'levelCompleted' if all challenges have been played
+  'levelCompleted'  // goes to 'levelSelection'
+] as const;
 export type GameState = ( typeof GameStateValues )[number];
