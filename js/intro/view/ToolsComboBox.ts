b92cca754ebb91a7e4d60976e581d3920774dbe6
pixelzoom <cmalley@pixelzoom.com>
2025-03-27 12:52:49 -0600

    replace icon images with new programmatic icons, https://github.com/phetsims/balancing-chemical-equations/issues/175

--- js/intro/view/ToolsComboBox.ts
+++ js/intro/view/ToolsComboBox.ts
@@ -9,18 +9,23 @@
 
 import StringUnionProperty from '../../../../axon/js/StringUnionProperty.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
-import Image from '../../../../scenery/js/nodes/Image.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
 import Text from '../../../../scenery/js/nodes/Text.js';
 import ComboBox, { ComboBoxItem } from '../../../../sun/js/ComboBox.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
-import charts_png from '../../../images/charts_png.js';
-import scales_png from '../../../mipmaps/scales_png.js';
 import balancingChemicalEquations from '../../balancingChemicalEquations.js';
 import BalancingChemicalEquationsStrings from '../../BalancingChemicalEquationsStrings.js';
 import { BalancedRepresentation } from '../../common/model/BalancedRepresentation.js';
+import Rectangle from '../../../../scenery/js/nodes/Rectangle.js';
+import Color from '../../../../scenery/js/util/Color.js';
+import HBox from '../../../../scenery/js/layout/nodes/HBox.js';
+import HStrut from '../../../../scenery/js/nodes/HStrut.js';
+import BalanceFulcrumNode from '../../common/view/BalanceFulcrumNode.js';
+import Dimension2 from '../../../../dot/js/Dimension2.js';
+import VBox from '../../../../scenery/js/layout/nodes/VBox.js';
+import ShadedSphereNode, { ShadedSphereNodeOptions } from '../../../../scenery-phet/js/ShadedSphereNode.js';
 
-const FONT = new PhetFont( 22 );
+const FONT = new PhetFont( 20 );
 
 export default class ToolsComboBox extends ComboBox<BalancedRepresentation> {
 
@@ -48,6 +53,7 @@ export default class ToolsComboBox extends ComboBox<BalancedRepresentation> {
 
     super( balanceRepresentationProperty, items, listboxParent, {
       isDisposable: false,
+      align: 'center',
       xMargin: 10,
       yMargin: 5,
       cornerRadius: 4,
@@ -58,11 +64,70 @@ export default class ToolsComboBox extends ComboBox<BalancedRepresentation> {
 }
 
 function createBalanceScales(): Node {
-  return new Image( scales_png, { scale: 0.1875 } );
+
+  // Atoms
+  const diameter = 12;
+  const shadedSphereNodeOptions: ShadedSphereNodeOptions = {
+    radius: 8,
+    mainColor: Color.grayColor( 180 ),
+    highlightColor: Color.grayColor( 240 ),
+    stroke: 'black',
+    lineWidth: 0.5
+  };
+  const atomsNode = new HBox( {
+    spacing: 0,
+    children: [
+      new ShadedSphereNode( diameter, shadedSphereNodeOptions ),
+      new ShadedSphereNode( diameter, shadedSphereNodeOptions ),
+      new HStrut( 40 ),
+      new ShadedSphereNode( diameter, shadedSphereNodeOptions ),
+      new ShadedSphereNode( diameter, shadedSphereNodeOptions )
+    ]
+  } );
+
+  // balance beam, in horizontal (balanced) orientation
+  const beamNode = new Rectangle( 0, 0, 135, 6, {
+    fill: 'black'
+  } );
+
+  const fulcrumNode = new BalanceFulcrumNode( {
+    // size: new Dimension2( 60, 45 ),
+    size: new Dimension2( 40, 30 ),
+    lineWidth: 1
+  } );
+
+  return new VBox( {
+    children: [ atomsNode, beamNode, fulcrumNode ],
+    spacing: 0,
+    align: 'center',
+    scale: 0.35
+  } );
 }
 
 function createBarChartsIcon(): Node {
-  return new Image( charts_png, { scale: 0.375 } );
+  const barWidth = 12;
+  const stroke = 'black';
+  const lineWidth = 0.5;
+  const rectangle1 = new Rectangle( 0, 0, barWidth, 10, {
+    fill: Color.grayColor( 109 ),
+    stroke: stroke,
+    lineWidth: lineWidth
+  } );
+  const rectangle2 = new Rectangle( 0, 0, barWidth, 15, {
+    fill: Color.grayColor( 217 ),
+    stroke: stroke,
+    lineWidth: lineWidth
+  } );
+  const rectangle3 = new Rectangle( 0, 0, barWidth, 20, {
+    fill: Color.grayColor( 163 ),
+    stroke: stroke,
+    lineWidth: lineWidth
+  } );
+  return new HBox( {
+    children: [ rectangle1, rectangle2, rectangle3 ],
+    spacing: 3,
+    align: 'bottom'
+  } );
 }
 
 balancingChemicalEquations.register( 'ToolsComboBox', ToolsComboBox );
