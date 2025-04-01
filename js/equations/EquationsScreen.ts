279b98022a70cfcceed26a5e723f21e27d54ee6d
pixelzoom <cmalley@pixelzoom.com>
2025-04-01 12:27:29 -0600

    Equations screen icon, https://github.com/phetsims/balancing-chemical-equations/issues/170

--- js/equations/EquationsScreen.ts
+++ js/equations/EquationsScreen.ts
@@ -17,6 +17,10 @@ import BalancingChemicalEquationsStrings from '../BalancingChemicalEquationsStri
 import BCEColors from '../common/BCEColors.js';
 import EquationsScreenView from './view/EquationsScreenView.js';
 import EquationsModel from './model/EquationsModel.js';
+import PhetFont from '../../../scenery-phet/js/PhetFont.js';
+import HBox from '../../../scenery/js/layout/nodes/HBox.js';
+import Text from '../../../scenery/js/nodes/Text.js';
+import ArrowNode from '../../../scenery-phet/js/ArrowNode.js';
 
 export default class EquationsScreen extends Screen<EquationsModel, EquationsScreenView> {
 
@@ -47,11 +51,29 @@ function createScreenIcon(): ScreenIcon {
   const height = Screen.MINIMUM_HOME_SCREEN_ICON_SIZE.height;
   const background = new Rectangle( 0, 0, width, height, { fill: 'white' } );
 
-  //TODO https://github.com/phetsims/balancing-chemical-equations/issues/170 Screen icon
-  const contentNode = new Rectangle( 0, 0, width, height, { fill: 'red' } );
+  // A + B -> AB
+  const textOptions = {
+    font: new PhetFont( { size: 24, weight: 'bold' } )
+  };
+  const equationNode = new HBox( {
+    children: [
+      new Text( 'A + B', textOptions ),
+      //TODO https://github.com/phetsims/balancing-chemical-equations/issues/170 Replace ArrowNode with '\u2B95' ?
+      new ArrowNode( 0, 0, 25, 0, {
+        stroke: null,
+        headWidth: 15
+      } ),
+      new Text( 'AB', textOptions )
+    ],
+    spacing: 5
+  } );
+
+  // scale equation to fit, center in background
+  equationNode.setScaleMagnitude( Math.min( 0.82 * background.width / equationNode.width, 0.82 * background.height / equationNode.height ) );
+  equationNode.center = background.center;
 
   const iconNode = new Node( {
-    children: [ background, contentNode ]
+    children: [ background, equationNode ]
   } );
 
   return new ScreenIcon( iconNode, {
