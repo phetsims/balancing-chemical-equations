74e9fb63728490231879dd9e534907103c2959f7
Chris Malley <cmalley@pixelzoom.com>
2025-04-03 10:48:52 -0600

    delete stale doc

--- js/common/model/Equation.ts
+++ js/common/model/Equation.ts
@@ -4,10 +4,6 @@
  * Equation is the base class for all chemical equations.
  * A chemical equation has 2 sets of terms, reactants and products.
  * During the chemical reaction represented by the equation, reactants are transformed into products.
- * <p/>
- * An equation is "balanced" when each term's user coefficient is an integer multiple N of
- * the balanced coefficient, N is the same for all terms in the equation, and N >= 1.
- * An equation is "balanced and simplified" when it is balanced and N=1.
  *
  * @author Vasily Shakhov (mlearner.com)
  * @author Chris Malley (PixelZoom, Inc.)
