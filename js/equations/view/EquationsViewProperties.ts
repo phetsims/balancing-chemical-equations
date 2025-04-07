9a6c18621904bd2e65def520ed0f50568fce8ee4
Chris Malley <cmalley@pixelzoom.com>
2025-04-07 16:21:05 -0600

    EquationsViewProperties extends IntroViewProperties, https://github.com/phetsims/balancing-chemical-equations/issues/170

--- js/equations/view/EquationsViewProperties.ts
+++ js/equations/view/EquationsViewProperties.ts
@@ -1,53 +1,20 @@
 // Copyright 2025, University of Colorado Boulder
 
-//TODO https://github.com/phetsims/balancing-chemical-equations/issues/170 identical to IntroViewProperties
 /**
- * EquationsViewProperties is the set of view-specific Properties for the 'Equations' screen
+ * EquationsViewProperties is the set of view-specific Properties for the 'Equations' screen. This is identical
+ * to IntroViewProperties and is provided only for completeness.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import StringUnionProperty from '../../../../axon/js/StringUnionProperty.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import balancingChemicalEquations from '../../balancingChemicalEquations.js';
-import { BalancedRepresentation, BalancedRepresentationValues } from '../../common/model/BalancedRepresentation.js';
-import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
-import Property from '../../../../axon/js/Property.js';
+import IntroViewProperties from '../../intro/view/IntroViewProperties.js';
 
-export default class EquationsViewProperties {
-
-  // Whether the 'Reactants' accordion box is expanded
-  public readonly reactantsAccordionBoxExpandedProperty: Property<boolean>;
-
-  // Whether the 'Products' accordion box is expanded
-  public readonly productsAccordionBoxExpandedProperty: Property<boolean>;
-
-  // The representation for 'balanced' that is chosen from ViewsComboBox.
-  public readonly balancedRepresentationProperty: StringUnionProperty<BalancedRepresentation>;
+export default class EquationsViewProperties extends IntroViewProperties {
 
   public constructor( tandem: Tandem ) {
-
-    this.reactantsAccordionBoxExpandedProperty = new BooleanProperty( true, {
-      tandem: tandem.createTandem( 'reactantsAccordionBoxExpandedProperty' ),
-      phetioFeatured: true
-    } );
-
-    this.productsAccordionBoxExpandedProperty = new BooleanProperty( true, {
-      tandem: tandem.createTandem( 'productsAccordionBoxExpandedProperty' ),
-      phetioFeatured: true
-    } );
-
-    this.balancedRepresentationProperty = new StringUnionProperty( 'balanceScales', {
-      validValues: BalancedRepresentationValues,
-      tandem: tandem.createTandem( 'balancedRepresentationProperty' ),
-      phetioFeatured: true
-    } );
-  }
-
-  public reset(): void {
-    this.reactantsAccordionBoxExpandedProperty.reset();
-    this.productsAccordionBoxExpandedProperty.reset();
-    this.balancedRepresentationProperty.reset();
+    super( tandem );
   }
 }
 
