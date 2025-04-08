a3814486cde77fbdaeb5103ec899fb723b1f5440
phet-dev <phetadmin@gmail.com>
2025-04-08 03:27:11 -0600

    update copyright dates from daily grunt work

--- js/common/view/BalanceElementsNode.ts
+++ js/common/view/BalanceElementsNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2014-2025, University of Colorado Boulder
 
 /**
  * BalanceElementsNode is the base class for BalancedRepresentation values that show how individual elements are
