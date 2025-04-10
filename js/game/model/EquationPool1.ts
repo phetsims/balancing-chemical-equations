0e71e066c859c9226eeefac15d623718bf5fc06b
pixelzoom <cmalley@pixelzoom.com>
2025-04-10 12:58:22 -0600

    factor out duplicated code

--- js/game/model/EquationPool1.ts
+++ js/game/model/EquationPool1.ts
@@ -19,51 +19,38 @@ export default class EquationPool1 extends EquationPool {
 
     let equationIndex = 0;
 
+    const create1Reactant2Products = ( r1: number, reactant1: Molecule, p1: number, product1: Molecule, p2: number, product2: Molecule ) =>
+      Equation.create1Reactant2Products( r1, reactant1, p1, product1, p2, product2, coefficientsRange,
+        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly );
+
+    const create2Reactants1Product = ( r1: number, reactant1: Molecule, r2: number, reactant2: Molecule, p1: number, product1: Molecule ) =>
+      Equation.create2Reactants1Product( r1, reactant1, r2, reactant2, p1, product1, coefficientsRange,
+        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly );
+
     const equations: Equation[] = [
       // This is the largest molecule. Put it first to simplify layout testing with ?playAll.
-      Equation.create1Reactant2Products( 1, Molecule.PCl5, 1, Molecule.PCl3, 1, Molecule.Cl2, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
+      create1Reactant2Products( 1, Molecule.PCl5, 1, Molecule.PCl3, 1, Molecule.Cl2 ),
       // This equation requires maxX adjustment in EquationNode. Put it here to simplify layout testing with ?playAll.
-      Equation.create1Reactant2Products( 1, Molecule.CH3OH, 1, Molecule.CO, 2, Molecule.H2, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create2Reactants1Product( 2, Molecule.H2, 1, Molecule.O2, 2, Molecule.H2O, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create2Reactants1Product( 1, Molecule.H2, 1, Molecule.F2, 2, Molecule.HF, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create1Reactant2Products( 2, Molecule.HCl, 1, Molecule.H2, 1, Molecule.Cl2, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create2Reactants1Product( 1, Molecule.CH2O, 1, Molecule.H2, 1, Molecule.CH3OH, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create1Reactant2Products( 1, Molecule.C2H6, 1, Molecule.C2H4, 1, Molecule.H2, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create2Reactants1Product( 1, Molecule.C2H2, 2, Molecule.H2, 1, Molecule.C2H6, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create2Reactants1Product( 1, Molecule.C, 1, Molecule.O2, 1, Molecule.CO2, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create2Reactants1Product( 2, Molecule.C, 1, Molecule.O2, 2, Molecule.CO, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create1Reactant2Products( 2, Molecule.CO2, 2, Molecule.CO, 1, Molecule.O2, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create1Reactant2Products( 2, Molecule.CO, 1, Molecule.C, 1, Molecule.CO2, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create2Reactants1Product( 1, Molecule.C, 2, Molecule.S, 1, Molecule.CS2, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create1Reactant2Products( 2, Molecule.NH3, 1, Molecule.N2, 3, Molecule.H2, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create1Reactant2Products( 2, Molecule.NO, 1, Molecule.N2, 1, Molecule.O2, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create1Reactant2Products( 2, Molecule.NO2, 2, Molecule.NO, 1, Molecule.O2, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create2Reactants1Product( 2, Molecule.N2, 1, Molecule.O2, 2, Molecule.N2O, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create2Reactants1Product( 1, Molecule.P4, 6, Molecule.H2, 4, Molecule.PH3, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create2Reactants1Product( 1, Molecule.P4, 6, Molecule.F2, 4, Molecule.PF3, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create1Reactant2Products( 4, Molecule.PCl3, 1, Molecule.P4, 6, Molecule.Cl2, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create1Reactant2Products( 2, Molecule.SO3, 2, Molecule.SO2, 1, Molecule.O2, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly )
+      create1Reactant2Products( 1, Molecule.CH3OH, 1, Molecule.CO, 2, Molecule.H2 ),
+      create2Reactants1Product( 2, Molecule.H2, 1, Molecule.O2, 2, Molecule.H2O ),
+      create2Reactants1Product( 1, Molecule.H2, 1, Molecule.F2, 2, Molecule.HF ),
+      create1Reactant2Products( 2, Molecule.HCl, 1, Molecule.H2, 1, Molecule.Cl2 ),
+      create2Reactants1Product( 1, Molecule.CH2O, 1, Molecule.H2, 1, Molecule.CH3OH ),
+      create1Reactant2Products( 1, Molecule.C2H6, 1, Molecule.C2H4, 1, Molecule.H2 ),
+      create2Reactants1Product( 1, Molecule.C2H2, 2, Molecule.H2, 1, Molecule.C2H6 ),
+      create2Reactants1Product( 1, Molecule.C, 1, Molecule.O2, 1, Molecule.CO2 ),
+      create2Reactants1Product( 2, Molecule.C, 1, Molecule.O2, 2, Molecule.CO ),
+      create1Reactant2Products( 2, Molecule.CO2, 2, Molecule.CO, 1, Molecule.O2 ),
+      create1Reactant2Products( 2, Molecule.CO, 1, Molecule.C, 1, Molecule.CO2 ),
+      create2Reactants1Product( 1, Molecule.C, 2, Molecule.S, 1, Molecule.CS2 ),
+      create1Reactant2Products( 2, Molecule.NH3, 1, Molecule.N2, 3, Molecule.H2 ),
+      create1Reactant2Products( 2, Molecule.NO, 1, Molecule.N2, 1, Molecule.O2 ),
+      create1Reactant2Products( 2, Molecule.NO2, 2, Molecule.NO, 1, Molecule.O2 ),
+      create2Reactants1Product( 2, Molecule.N2, 1, Molecule.O2, 2, Molecule.N2O ),
+      create2Reactants1Product( 1, Molecule.P4, 6, Molecule.H2, 4, Molecule.PH3 ),
+      create2Reactants1Product( 1, Molecule.P4, 6, Molecule.F2, 4, Molecule.PF3 ),
+      create1Reactant2Products( 4, Molecule.PCl3, 1, Molecule.P4, 6, Molecule.Cl2 ),
+      create1Reactant2Products( 2, Molecule.SO3, 2, Molecule.SO2, 1, Molecule.O2 )
     ];
 
     super( equations, {
--- js/game/model/EquationPool2.ts
+++ js/game/model/EquationPool2.ts
@@ -19,29 +19,23 @@ export default class EquationPool2 extends EquationPool {
 
     let equationIndex = 0;
 
+    const create2Reactants2Products = ( r1: number, reactant1: Molecule, r2: number, reactant2: Molecule,
+                                        p1: number, product1: Molecule, p2: number, product2: Molecule ) =>
+      Equation.create2Reactants2Products( r1, reactant1, r2, reactant2, p1, product1, p2, product2, coefficientsRange,
+        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly );
+
     const equations: Equation[] = [
-      Equation.create2Reactants2Products( 2, Molecule.C, 2, Molecule.H2O, 1, Molecule.CH4, 1, Molecule.CO2, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create2Reactants2Products( 1, Molecule.CH4, 1, Molecule.H2O, 3, Molecule.H2, 1, Molecule.CO, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create2Reactants2Products( 1, Molecule.CH4, 2, Molecule.O2, 1, Molecule.CO2, 2, Molecule.H2O, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create2Reactants2Products( 1, Molecule.C2H4, 3, Molecule.O2, 2, Molecule.CO2, 2, Molecule.H2O, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create2Reactants2Products( 1, Molecule.C2H6, 1, Molecule.Cl2, 1, Molecule.C2H5Cl, 1, Molecule.HCl, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create2Reactants2Products( 1, Molecule.CH4, 4, Molecule.S, 1, Molecule.CS2, 2, Molecule.H2S, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create2Reactants2Products( 1, Molecule.CS2, 3, Molecule.O2, 1, Molecule.CO2, 2, Molecule.SO2, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create2Reactants2Products( 1, Molecule.SO2, 2, Molecule.H2, 1, Molecule.S, 2, Molecule.H2O, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create2Reactants2Products( 1, Molecule.SO2, 3, Molecule.H2, 1, Molecule.H2S, 2, Molecule.H2O, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create2Reactants2Products( 2, Molecule.F2, 1, Molecule.H2O, 1, Molecule.OF2, 2, Molecule.HF, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly ),
-      Equation.create2Reactants2Products( 1, Molecule.OF2, 1, Molecule.H2O, 1, Molecule.O2, 2, Molecule.HF, coefficientsRange,
-        tandem.createTandem( `equation${equationIndex++}` ), EquationPool.coefficientPropertyPhetioReadOnly )
+      create2Reactants2Products( 2, Molecule.C, 2, Molecule.H2O, 1, Molecule.CH4, 1, Molecule.CO2 ),
+      create2Reactants2Products( 1, Molecule.CH4, 1, Molecule.H2O, 3, Molecule.H2, 1, Molecule.CO ),
+      create2Reactants2Products( 1, Molecule.CH4, 2, Molecule.O2, 1, Molecule.CO2, 2, Molecule.H2O ),
+      create2Reactants2Products( 1, Molecule.C2H4, 3, Molecule.O2, 2, Molecule.CO2, 2, Molecule.H2O ),
+      create2Reactants2Products( 1, Molecule.C2H6, 1, Molecule.Cl2, 1, Molecule.C2H5Cl, 1, Molecule.HCl ),
+      create2Reactants2Products( 1, Molecule.CH4, 4, Molecule.S, 1, Molecule.CS2, 2, Molecule.H2S ),
+      create2Reactants2Products( 1, Molecule.CS2, 3, Molecule.O2, 1, Molecule.CO2, 2, Molecule.SO2 ),
+      create2Reactants2Products( 1, Molecule.SO2, 2, Molecule.H2, 1, Molecule.S, 2, Molecule.H2O ),
+      create2Reactants2Products( 1, Molecule.SO2, 3, Molecule.H2, 1, Molecule.H2S, 2, Molecule.H2O ),
+      create2Reactants2Products( 2, Molecule.F2, 1, Molecule.H2O, 1, Molecule.OF2, 2, Molecule.HF ),
+      create2Reactants2Products( 1, Molecule.OF2, 1, Molecule.H2O, 1, Molecule.O2, 2, Molecule.HF )
     ];
 
     super( equations, {
