b8535f332bc543f0db8521ef644d11ddc1d88bc0
phet-dev <phetadmin@gmail.com>
2026-02-18 03:34:29 -0700

    update copyright dates from daily grunt work

--- js/equations/view/ReactionTypeRadioButtonGroup.ts
+++ js/equations/view/ReactionTypeRadioButtonGroup.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 
 /**
  * ReactionTypeRadioButtonGroup is the radio button group for selecting a reaction type on the 'Equations' screen.
